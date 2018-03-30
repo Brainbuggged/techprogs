62758a8c267e678e4fb217367229f3b4fea49f54
Alex Chernyshev <brainbuggged@gmail.com>
2018-03-30 19:22:58 +0300

    COM objects

---
+++ TechProg2/TechProg2/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace TechProg2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axShockwaveFlash1 = new AxShockwaveFlashObjects.AxShockwaveFlash();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.axShockwaveFlash1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axShockwaveFlash1
+            // 
+            this.axShockwaveFlash1.Enabled = true;
+            this.axShockwaveFlash1.Location = new System.Drawing.Point(486, 29);
+            this.axShockwaveFlash1.Name = "axShockwaveFlash1";
+            this.axShockwaveFlash1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axShockwaveFlash1.OcxState")));
+            this.axShockwaveFlash1.Size = new System.Drawing.Size(222, 229);
+            this.axShockwaveFlash1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(28, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(452, 354);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(981, 414);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.axShockwaveFlash1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axShockwaveFlash1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxShockwaveFlashObjects.AxShockwaveFlash axShockwaveFlash1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ TechProg2/TechProg2/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TPLab2;
+
+
+
+namespace TechProg2
+{
+    public partial class Form1 : Form, IOleClientSite, IOleInPlaceSite
+    {
+        tagRECT NewRect = new tagRECT(100, 100, 100, 100);
+        tagRECT New = new tagRECT(0, 0, 500, 400);
+        public Form1()
+        {
+           OLE.CoInitializeEx(IntPtr.Zero, 2);
+            
+            InitializeComponent();
+
+
+
+            axShockwaveFlash1.Movie = Application.StartupPath + @"\tuma.swf";
+
+
+
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+            object browser = OLE.CoCreateInstance(OLE.CLASS_WEBBROWSER, null,
+                  OLE.CLSCTX.CLSCTX_INPROC_SERVER,
+                  OLE.IID_IWebBrowser2);
+            IOleObject obj = browser as IOleObject;
+            tagRECT rect = new tagRECT();
+            obj.SetClientSite(this);
+            obj.DoVerb((int)OLEDOVERB.OLEIVERB_INPLACEACTIVATE,
+                IntPtr.Zero, this, 0, Handle, ref rect
+                );
+            IWebBrowser2 brow = browser as IWebBrowser2;
+            (brow as IWebBrowser2).Navigate("http://ya.ru", null, null, null, null);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        [return: MarshalAs(UnmanagedType.I4)]
+        public int SaveObject()
+        {
+            return HRESULT.S_OK;
+        }
+
+        [return: MarshalAs(UnmanagedType.I4)]
+        public int GetMoniker([In, MarshalAs(UnmanagedType.U4)] uint dwAssign, [In, MarshalAs(UnmanagedType.U4)] uint dwWhichMoniker, [MarshalAs(UnmanagedType.Interface), Out] out IMoniker ppmk)
+        {
+            ppmk = null;
+            return HRESULT.E_FAIL;
+        }
+
+        [return: MarshalAs(UnmanagedType.I4)]
+        public int GetContainer([MarshalAs(UnmanagedType.Interface), Out] out IOleContainer ppContainer)
+        {
+            ppContainer = null;
+            return HRESULT.E_NOINTERFACE;
+        }
+
+        [return: MarshalAs(UnmanagedType.I4)]
+        public int ShowObject()
+        {
+            return HRESULT.S_OK;
+        }
+
+        [return: MarshalAs(UnmanagedType.I4)]
+        public int OnShowWindow([In, MarshalAs(UnmanagedType.Bool)] bool fShow)
+        {
+            return HRESULT.S_OK;
+        }
+
+        [return: MarshalAs(UnmanagedType.I4)]
+        public int RequestNewObjectLayout()
+        {
+            return HRESULT.S_OK;
+        }
+
+        [return: MarshalAs(UnmanagedType.I4)]
+        public int GetWindow([In, Out] ref IntPtr phwnd)
+        {
+            phwnd = groupBox1.Handle;
+            return HRESULT.S_OK;
+        }
+
+        [return: MarshalAs(UnmanagedType.I4)]
+        public int ContextSensitiveHelp([In, MarshalAs(UnmanagedType.Bool)] bool fEnterMode)
+        {
+            return HRESULT.S_OK;
+        }
+
+        [return: MarshalAs(UnmanagedType.I4)]
+        public int CanInPlaceActivate()
+        {
+            return HRESULT.S_OK;
+        }
+
+        [return: MarshalAs(UnmanagedType.I4)]
+        public int OnInPlaceActivate()
+        {
+            return HRESULT.S_OK;
+        }
+
+        [return: MarshalAs(UnmanagedType.I4)]
+        public int OnUIActivate()
+        {
+            return HRESULT.S_OK;
+        }
+        [return: MarshalAs(UnmanagedType.I4)]
+        public int GetWindowContext([MarshalAs(UnmanagedType.Interface), Out] out IOleInPlaceFrame ppFrame, [MarshalAs(UnmanagedType.Interface), Out] out IOleInPlaceUIWindow ppDoc, [In, MarshalAs(UnmanagedType.Struct), Out] ref tagRECT lprcPosRect, [In, MarshalAs(UnmanagedType.Struct), Out] ref tagRECT lprcClipRect, [In, MarshalAs(UnmanagedType.Struct), Out] ref tagOIFI lpFrameInfo)
+        {
+            ppFrame = null;
+            ppDoc = null;
+            lprcPosRect = New;
+            lprcClipRect = New;
+
+            return HRESULT.S_OK;
+        }
+
+        int IOleInPlaceSite.Scroll(ref tagSIZE scrollExtent)
+        {
+            return HRESULT.S_OK;
+        }
+
+        [return: MarshalAs(UnmanagedType.I4)]
+        public int OnUIDeactivate([In, MarshalAs(UnmanagedType.Bool)] bool fUndoable)
+        {
+            return HRESULT.S_OK;
+        }
+
+        [return: MarshalAs(UnmanagedType.I4)]
+        public int OnInPlaceDeactivate()
+        {
+            return HRESULT.S_OK;
+        }
+
+        [return: MarshalAs(UnmanagedType.I4)]
+        public int DiscardUndoState()
+        {
+            return HRESULT.S_OK;
+        }
+
+        [return: MarshalAs(UnmanagedType.I4)]
+        public int DeactivateAndUndo()
+        {
+            return HRESULT.S_OK;
+        }
+
+        [return: MarshalAs(UnmanagedType.I4)]
+        public int OnPosRectChange([In, MarshalAs(UnmanagedType.Struct)] ref tagRECT lprcPosRect)
+        {
+            lprcPosRect = New;
+            return HRESULT.S_OK;
+        }
+
+       
+    }
+}

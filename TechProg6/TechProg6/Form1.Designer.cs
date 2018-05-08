640ae1df49c6e2ef3521b02cdca06a8359d071eb
WIN-1V1UH9JC45K\User <brainbuggged@gmail.com>
2018-05-08 20:57:58 +0300

    controls are renamed

--- TechProg6/TechProg6/Form1.Designer.cs
+++ TechProg6/TechProg6/Form1.Designer.cs
@@ -28,69 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.inputTxtBox = new System.Windows.Forms.RichTextBox();
+            this.task1Btn = new System.Windows.Forms.Button();
+            this.outputTxtBox = new System.Windows.Forms.TextBox();
+            this.task2Btn = new System.Windows.Forms.Button();
+            this.task3Btn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // richTextBox1
+            // inputTxtBox
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(29, 40);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(291, 58);
-            this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "";
+            this.inputTxtBox.Location = new System.Drawing.Point(29, 40);
+            this.inputTxtBox.Name = "inputTxtBox";
+            this.inputTxtBox.Size = new System.Drawing.Size(291, 58);
+            this.inputTxtBox.TabIndex = 0;
+            this.inputTxtBox.Text = "";
             // 
-            // button1
+            // task1Btn
             // 
-            this.button1.Location = new System.Drawing.Point(34, 246);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Check1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            this.task1Btn.Location = new System.Drawing.Point(29, 246);
+            this.task1Btn.Name = "task1Btn";
+            this.task1Btn.Size = new System.Drawing.Size(75, 23);
+            this.task1Btn.TabIndex = 1;
+            this.task1Btn.Text = "Check1";
+            this.task1Btn.UseVisualStyleBackColor = true;
+            this.task1Btn.Click += new System.EventHandler(this.Button1_Click);
             // 
-            // textBox1
+            // outputTxtBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(29, 117);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(291, 123);
-            this.textBox1.TabIndex = 2;
+            this.outputTxtBox.Location = new System.Drawing.Point(29, 117);
+            this.outputTxtBox.Multiline = true;
+            this.outputTxtBox.Name = "outputTxtBox";
+            this.outputTxtBox.Size = new System.Drawing.Size(291, 123);
+            this.outputTxtBox.TabIndex = 2;
             // 
-            // button2
+            // task2Btn
             // 
-            this.button2.Location = new System.Drawing.Point(139, 246);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Check2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            this.task2Btn.Location = new System.Drawing.Point(139, 246);
+            this.task2Btn.Name = "task2Btn";
+            this.task2Btn.Size = new System.Drawing.Size(75, 23);
+            this.task2Btn.TabIndex = 3;
+            this.task2Btn.Text = "Check2";
+            this.task2Btn.UseVisualStyleBackColor = true;
+            this.task2Btn.Click += new System.EventHandler(this.Button2_Click);
             // 
-            // button3
+            // task3Btn
             // 
-            this.button3.Location = new System.Drawing.Point(250, 246);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Check3";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            this.task3Btn.Location = new System.Drawing.Point(245, 246);
+            this.task3Btn.Name = "task3Btn";
+            this.task3Btn.Size = new System.Drawing.Size(75, 23);
+            this.task3Btn.TabIndex = 4;
+            this.task3Btn.Text = "Check3";
+            this.task3Btn.UseVisualStyleBackColor = true;
+            this.task3Btn.Click += new System.EventHandler(this.Button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(352, 311);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.task3Btn);
+            this.Controls.Add(this.task2Btn);
+            this.Controls.Add(this.outputTxtBox);
+            this.Controls.Add(this.task1Btn);
+            this.Controls.Add(this.inputTxtBox);
             this.Name = "Form1";
             this.Text = "Regex";
             this.ResumeLayout(false);
@@ -100,11 +100,11 @@
 
         #endregion
 
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox inputTxtBox;
+        private System.Windows.Forms.Button task1Btn;
+        private System.Windows.Forms.TextBox outputTxtBox;
+        private System.Windows.Forms.Button task2Btn;
+        private System.Windows.Forms.Button task3Btn;
     }
 }
 
--- TechProg6/TechProg6/Form1.cs
+++ TechProg6/TechProg6/Form1.cs
@@ -13,52 +13,45 @@ namespace TechProg6
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "";
+            outputTxtBox.Text = "";
             var rTest = new Regex("[0-7]");
             var lText = new Regex("[a-zA-Z]");
             var eightText = new Regex("[8-9]");
 
-            if (richTextBox1.Text != "")
+            if (inputTxtBox.Text != "")
             {
-                if (rTest.IsMatch(richTextBox1.Text) && !lText.IsMatch(richTextBox1.Text)
-                    && !eightText.IsMatch(richTextBox1.Text))
-                    textBox1.AppendText("Корректное восьмеричное число\r\n");
+                if (rTest.IsMatch(inputTxtBox.Text) && !lText.IsMatch(inputTxtBox.Text)
+                    && !eightText.IsMatch(inputTxtBox.Text))
+                    outputTxtBox.AppendText("Корректное восьмеричное число\r\n");
                 else
-                    textBox1.AppendText("Некорректное восьмеричное число\r\n");
+                    outputTxtBox.AppendText("Некорректное восьмеричное число\r\n");
             }
-            else { textBox1.Text = @"Обнаружено пустое поле"; }
+            else { outputTxtBox.Text = @"Обнаружено пустое поле"; }
 
         }
 
         private void Button2_Click(object sender, EventArgs e)
         {
-
-
-            textBox1.Text = "";
+            outputTxtBox.Text = "";
             var rTest = new Regex("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+[a-zA-Z]{2,4}");
 
-            if (richTextBox1.Text != "")
+            if (inputTxtBox.Text != "")
             {
-                textBox1.AppendText(rTest.IsMatch(richTextBox1.Text)
+                outputTxtBox.AppendText(rTest.IsMatch(inputTxtBox.Text)
                     ? "Корректный электронный адрес\r\n"
                     : "Некорректный электронный адрес\r\n");
             }
-            else { textBox1.Text = @"Обнаружено пустое поле"; }
+            else { outputTxtBox.Text = @"Обнаружено пустое поле"; }
         }
     
         private void Button3_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "";
-         
+            outputTxtBox.Text = "";
             var passTest = new Regex(@"^[^)(1-9,\.\\\@\'\""{}<>:;*&'/]{1,}[0-9a-zA-Z_]{1,}$");
-
-            //var passTest1 = new Regex(@"^[\D_]*[\d]*");
-            foreach (Match m in passTest.Matches(richTextBox1.Text))
+            foreach (Match m in passTest.Matches(inputTxtBox.Text))
             {
-                textBox1.AppendText(m.Value + "\n");
+                outputTxtBox.AppendText(m.Value + "\n");
             }
-
-
         }
     }
 }

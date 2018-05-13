3d078a7307cad519f8c63ca071dda7c0dc765537
WIN-1V1UH9JC45K\User <brainbuggged@gmail.com>
2018-05-13 22:38:43 +0300

    remove unused libraries

--- TechProg1/TechProg1/Class2.cs
+++ TechProg1/TechProg1/Class2.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MynameSpace
+﻿namespace MynameSpace
 {
     internal class Class2
     {
